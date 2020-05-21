ae471d38a7ccaf4b81681d12babae4417d284deb
Lott, Christopher (cl778h) <cl778h@att.com>
2020-05-21 16:46:44 -0400

    Mock QoE xApp that sends a constant prediction
    Version 0.0.1 of an xApplication based on the Python xApp framework.
    This mockup listens for a QoE prediction request (RMR message type
    30001) as sent by the QP Driver xApp.  Reacts by sending a fixed QoE
    prediction (RMR message type 30002), which should be routed to the
    Traffic Steering xApp.
    
    Includes a static RMR routing table with this host name for TS:
    service-ricxapp-trafficxapp-rmr.svc.cluster.local.
    
    Issue-ID: RICAPP-107
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ie6a6a851dff87c23a0bcaf23d9936e35d0cb3dd9

---
+++ setup.py
@@ -0,0 +1,28 @@
+# ==================================================================================
+#       Copyright (c) 2020 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ==================================================================================
+from setuptools import setup, find_packages
+
+setup(
+    name="qp",
+    version="0.0.1",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    description="Quality-of-Service Predictor Xapp for Traffic Steering",
+    url="https://gerrit.o-ran-sc.org/r/admin/repos/ric-app/qp",
+    install_requires=["ricxappframe>=1.1.1,<2.0.0"],
+    entry_points={"console_scripts": ["run-qp.py=qp.main:start"]},  # adds a magical entrypoint for Docker
+    license="Apache 2.0",
+    data_files=[("", ["LICENSE.txt"])],
+)
